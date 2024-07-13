254d3ca16878196163f6c8487fca620dcb18ca56
alecbnono <alecbnono@gmail.com>
2024-07-14 02:04:09 +0800

    Reapply "Testing"
    This reverts commit db8b5147b94e746ef3eb02f05b0a91fcab58692c.

---
+++ server/db/index.js
@@ -0,0 +1,12 @@
+import pg from "pg";
+const { Pool } = pg;
+
+const pool = new Pool();
+
+const query = (text, params, callback) => {
+    return pool.query(text, params, callback);
+};
+
+export default {
+    query,
+};
